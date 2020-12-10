de049869a2a6e3b767330e24a10238b8a9726b14
kasper023 <megasamurai02062000@gmail.com>
2020-12-10 20:41:45 +0600

    interval

--- slider/index.js
+++ slider/index.js
@@ -1,9 +1,15 @@
 let left = 0
 let slider = document.getElementById('slider')
+let interval
 function slider_move(val) {
-    console.log('move');
+    if(val === undefined) {
+        val = -200
+    }
     left += val
-    if(left < -400) left = -400
-    if(left > 0) left = 0
+    if(left < -400) left = 0
+    if(left > 0) left = -400
     slider.style.left = left + 'px'
+    clearInterval(interval)
+    interval = setInterval(slider_move, 5000);
 }
+interval = setInterval(slider_move, 5000);
